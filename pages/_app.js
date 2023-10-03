f4b5f59e9e7ab0a1f6b27e7dad14ee7b2bc097df
Hasnain Sayyed <hasnainsayyed@Hasnains-MacBook-Air.local>
2023-10-03 13:30:00 +0530

    redux setup

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,46 @@
 import "../styles/globals.css";
-
+import { useDispatch } from "react-redux";
+import { loginSuccess } from "../reducer";
+import { auth } from "../utils/Firebase";
+import { wrapper, store } from "../reducer/user";
+import { Provider } from "react-redux";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const dispatch = useDispatch();
+  React.useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        await db
+          .collection("users")
+          .doc(user.email)
+          .get()
+          .then((doc) => {
+            if (doc && doc.exists) {
+              var separatedString1 = doc.data();
+              dispatch(
+                loginSuccess({
+                  name: separatedString1.name,
+                  email: separatedString1.email,
+                  url: separatedString1.url,
+                  date: separatedString1.date,
+                  role: separatedString1.role,
+                })
+              );
+            } else {
+              dispatch(loginSuccess({}));
+            }
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+      }
+    });
+    return () => unsubscribe();
+  }, [dispatch]);
+  return (
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp;
+export default wrapper.withRedux(MyApp);
